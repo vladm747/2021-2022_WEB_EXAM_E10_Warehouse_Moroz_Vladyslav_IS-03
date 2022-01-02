cd5800958f997601dac7c83d7f182e565dbb080a
vladm747 <vladyslav.moroz105@gmail.com>
2022-01-02 14:57:47 +0200

    Add project files.

---
+++ E10_Warehouse/Models/Good.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E10_Warehouse.Models
+{
+    public class Good
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Не вказана назва")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Довжина назви має бути від 3 до 20 символів")]
+        public string GoodName { get; set; }
+        [Required]
+        [RegularExpression(@"\-?\d+(\.\d{0,})?", ErrorMessage = "Має вводитися число")]
+        public int Quantity { get; set; }
+        public bool IsAvailible { get; set; }
+    }
+}
---
+++ E10_Warehouse/Models/GoodsContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace E10_Warehouse.Models
+{
+    public class GoodsContext: DbContext
+    {
+        public GoodsContext(DbContextOptions<GoodsContext> options)
+          : base(options)
+        {
+            Database.EnsureCreated();   // создаем базу данных при первом обращении
+        }
+        DbSet<Good> Goods { get; set; }
+    }
+}
